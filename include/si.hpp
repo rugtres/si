82543c44b715daff9d9e520e195c7470e35d7207
Hanno Hildenbrandt <h.hildenbrandt@rug.nl>
2021-10-20 15:47:08 +0200

    cleaned up a bit
    Signed-off-by: Hanno Hildenbrandt <h.hildenbrandt@rug.nl>

--- include/si.hpp
+++ include/si.hpp
@@ -1,17 +1,34 @@
 #pragma once
 
+#include <type_traits>
+#include <string>
+
+
 namespace si {
 
-  // a quantity with unit [Mass^M * Length^L * time^T]
+  // quantity_t
+  // a value with unit [Mass^M * Length^L * Time^T]
+  // 
+  // thus our base quantities are: 
+  //   Mass = quantity_t<1, 0, 0>;
+  //   Length = quantity_t<0, 1, 0>;
+  //   Time = quantity_t<0, 0, 1>;
+  //
+  // Current, Temperature, 'amount of substance' and Luminosity are left as an exercise
+  //
   template <int M, int L, int T>
   class quantity_t {
+  private:
+    double value_;
+
   public:
     constexpr quantity_t() : value_(0.0) {}
     constexpr quantity_t(double val) : value_(val) {}
 
+    explicit operator double();
+
     constexpr double value() const { return value_; }
 
-    // addition and subtraction doesn't change units
     constexpr const quantity_t& operator+=(const quantity_t& rhs) {
       value_ += rhs.value_;
     }
@@ -19,23 +36,52 @@ namespace si {
     constexpr const quantity_t& operator-=(const quantity_t& rhs) {
       value_ -= rhs.value_;
     }
-
-  private:
-    double value_;
   };
 
+  // explicit conversion to double is fine iff M==L==T==0
+  template <>
+  quantity_t<0, 0, 0>::operator double() {
+    return value_;
+  }
 
-  // base quantities
-  using Mass = quantity_t<1, 0, 0>;
-  using Length = quantity_t<0, 1, 0>;
-  using Time = quantity_t<0, 0, 1>;
 
+  // comparison operators, only defined for equal units
+
+  template <int M, int L, int T>
+  inline constexpr bool operator == (const quantity_t<M, L, T>& lhs, const quantity_t<M, L, T>& rhs) {
+    return (lhs.value() == rhs.value());
+  }
+
+  template <int M, int L, int T>
+  inline constexpr bool operator != (const quantity_t<M, L, T>& lhs, const quantity_t<M, L, T>& rhs) {
+    return (lhs.value() != rhs.value());
+  }
+
+  template <int M, int L, int T>
+  inline constexpr bool operator <= (const quantity_t<M, L, T>& lhs, const quantity_t<M, L, T>& rhs) {
+    return (lhs.value() <= rhs.value());
+  }
+
+  template <int M, int L, int T>
+  inline constexpr bool operator >= (const quantity_t<M, L, T>& lhs, const quantity_t<M, L, T>& rhs) {
+    return (lhs.value() >= rhs.value());
+  }
+
+  template <int M, int L, int T>
+  inline constexpr bool operator < (const quantity_t<M, L, T>& lhs, const quantity_t<M, L, T>& rhs) {
+    return (lhs.value() < rhs.value());
+  }
+
+  template <int M, int L, int T>
+  inline constexpr bool operator > (const quantity_t<M, L, T>& lhs, const quantity_t<M, L, T>& rhs) {
+    return (lhs.value() > rhs.value());
+  }
 
   // addition of homogeneous quantities
   // units are unchanged
 
   template <int M, int L, int T>
-  constexpr auto operator + (const quantity_t<M, L, T>& lhs, const quantity_t<M, L, T>& rhs) {
+  inline constexpr auto operator + (const quantity_t<M, L, T>& lhs, const quantity_t<M, L, T>& rhs) {
     return quantity_t<M, L, T>(lhs.value() + rhs.value());
   }
 
@@ -43,7 +89,7 @@ namespace si {
   // units are unchanged 
 
   template <int M, int L, int T>
-  constexpr auto operator - (const quantity_t<M, L, T>& lhs, const quantity_t<M, L, T>& rhs) {
+  inline constexpr auto operator - (const quantity_t<M, L, T>& lhs, const quantity_t<M, L, T>& rhs) {
     return quantity_t<M, L, T>(lhs.value() - rhs.value());
   }
 
@@ -51,17 +97,17 @@ namespace si {
   // units are unchanged
 
   template <int M, int L, int T>
-  constexpr auto operator * (const double& lhs, const quantity_t<M, L, T>& rhs) {
+  inline constexpr auto operator * (const double& lhs, const quantity_t<M, L, T>& rhs) {
     return quantity_t<M, L, T>(lhs * rhs.value());
   }
 
   template <int M, int L, int T>
-  constexpr auto operator * (const quantity_t<M, L, T>& lhs, const double& rhs) {
+  inline constexpr auto operator * (const quantity_t<M, L, T>& lhs, const double& rhs) {
     return quantity_t<M, L, T>(lhs.value() * rhs);
   }
 
   template <int M, int L, int T>
-  constexpr auto operator / (quantity_t<M, L, T>& lhs, const double& rhs) {
+  inline constexpr auto operator / (const quantity_t<M, L, T>& lhs, const double& rhs) {
     return quantity_t<M, L, T>(lhs.value() / rhs);
   }
 
@@ -69,66 +115,35 @@ namespace si {
   // division number / quantity
   // units flip sign!
   template <int M, int L, int T>
-  constexpr auto operator / (const double& lhs, const quantity_t<M, L, T>& rhs) {
-    return quantity_t<-M, -L, -T>(
-      lhs / rhs.value()
-    );
+  inline constexpr auto operator / (const double& lhs, const quantity_t<M, L, T>& rhs) {
+    return quantity_t<-M, -L, -T>(lhs / rhs.value());
   }
 
   // mixed quantity multiplication
   // units are added together!
   template <int ML, int LL, int TL,
             int MR, int LR, int TR>
-  constexpr auto operator * (const quantity_t<ML, LL, TL>& lhs, const quantity_t<MR, LR, TR>& rhs) {
-    return quantity_t<ML + MR, LL + LL, TL + TR>(
+  inline constexpr auto operator * (const quantity_t<ML, LL, TL>& lhs, const quantity_t<MR, LR, TR>& rhs) {
+    return quantity_t<ML + MR, LL + LR, TL + TR>(
       lhs.value() * rhs.value()
     );
   }
 
-  // mixed quantity multiplication
+  // mixed quantity division
   // units are subtracted!
   template <int ML, int LL, int TL,
             int MR, int LR, int TR>
-  constexpr auto operator / (const quantity_t<ML, LL, TL>& lhs, const quantity_t<MR, LR, TR>& rhs) {
+  inline constexpr auto operator / (const quantity_t<ML, LL, TL>& lhs, const quantity_t<MR, LR, TR>& rhs) {
     return quantity_t<ML - MR, LL - LR, TL - TR>(
       lhs.value() / rhs.value()
     );
   }
 
-  // ...
 
-  // comparison operators
-
-  template <int M, int L, int T>
-  constexpr bool operator==(const quantity_t<M, L, T>& lhs, const quantity_t<M, L, T>& rhs) {
-    return (lhs.value() == rhs.value());
-  }
-
-  template <int M, int L, int T>
-  constexpr bool operator!=(const quantity_t<M, L, T>& lhs, const quantity_t<M, L, T>& rhs) {
-    return (lhs.value() != rhs.value());
-  }
-
-  template <int M, int L, int T>
-  constexpr bool operator<=(const quantity_t<M, L, T>& lhs, const quantity_t<M, L, T>& rhs) {
-    return (lhs.value() <= rhs.value());
-  }
-
-  template <int M, int L, int T>
-  constexpr bool operator>=(const quantity_t<M, L, T>& lhs, const quantity_t<M, L, T>& rhs) {
-    return (lhs.value() >= rhs.value());
-  }
-
-  template <int M, int L, int T>
-  constexpr bool operator<(const quantity_t<M, L, T>& lhs, const quantity_t<M, L, T>& rhs) {
-    return (lhs.value() < rhs.value());
-  }
-
-  template <int M, int L, int T>
-  constexpr bool operator>(const quantity_t<M, L, T>& lhs, const quantity_t<M, L, T>& rhs) {
-    return (lhs.value() > rhs.value());
-  }
-
+  // base quantities
+  using Mass = quantity_t<1, 0, 0>;
+  using Length = quantity_t<0, 1, 0>;
+  using Time = quantity_t<0, 0, 1>;
 
   // si base units
   constexpr auto Kilogram  = Mass(1.0);
@@ -140,21 +155,31 @@ namespace si {
   constexpr auto Kilometer = Length(1000.0);
   constexpr auto Hour = Time(60.0*60.0);
 
-  // a derived quantity and its unit
+  // derived quantities and optionally their si units
+  using Speed = quantity_t<0, 1, -1>;
   using Force = quantity_t<1, 1, -2>;
+  using Energy = quantity_t<1, 2, -2>;
   constexpr auto Newton = Force(1.0);
+  constexpr auto Joule = Energy(1.0);
 
- // ... and their literals
+  // ... and their literals
 #define si_define_literal(quant, lit, name) \
-constexpr quant operator"" lit(long double x) { return static_cast<double>(x) * name; }; \
-constexpr quant operator"" lit(unsigned long long x) { return static_cast<double>(x) * name; };
+  constexpr quant operator"" lit(long double x) { return static_cast<double>(x) * name; }; \
+  constexpr quant operator"" lit(unsigned long long x) { return static_cast<double>(x) * name; };
 
-si_define_literal(Mass, _kg, Kilogram);
-si_define_literal(Length, _m, Meter);
-si_define_literal(Time, _s, Second);
-si_define_literal(Mass, _g, Gram);
-si_define_literal(Length, _km, Kilometer);
-si_define_literal(Time, _h, Hour);
-si_define_literal(Force, _N, Newton);
+  si_define_literal(Mass, _kg, Kilogram);
+  si_define_literal(Length, _m, Meter);
+  si_define_literal(Time, _s, Second);
+  si_define_literal(Mass, _g, Gram);
+  si_define_literal(Length, _km, Kilometer);
+  si_define_literal(Time, _h, Hour);
+  si_define_literal(Force, _N, Newton);
+  si_define_literal(Energy, _J, Joule);
+
+
+  template <int M, int L, int T>
+  inline auto unit_str(const quantity_t<M, L, T>& q) {
+    return (std::string("[kg^") + std::to_string(M) + " m^" + std::to_string(L) + " s^" + std::to_string(T) + "]");
+  }
 
 }
--- test/si.cpp
+++ test/si.cpp
@@ -1,17 +1,41 @@
+#include <type_traits>
 #include <iostream>
 #include <si.hpp>
 
 using namespace si;
 
 
+Energy Ekin(Mass m, Speed v) {
+  return 0.5 * m * (v * v);
+}
+
+
 int main() {
   auto slow = 10_km / 1_h;
   auto fast = 10_m / 1_s;
-  
-  std::cout << "the slow speed is: " << slow.value() << " m/s\n";
-  std::cout << "the fast speed is: " << fast.value() << " m/s\n";
-  std::cout << "slow < fast " << std::boolalpha << (slow < fast) << std::endl;
-  //Mass M = fast;
-      
+
+  std::cout << "slow: " << slow.value() << ' ' << unit_str(slow) << '\n';
+  std::cout << "fast: " << fast.value() << ' ' << unit_str(fast) << '\n';
+  std::cout << "slow < fast " << std::boolalpha << (slow < fast) << '\n';
+
+  auto ekin = Ekin(80_kg, fast);
+  std::cout << "ekin = " << ekin.value() << " " << unit_str(ekin) << '\n';
+  std::cout << "ekin == 4000 J: " << std::boolalpha << (ekin == 4000_J) << '\n';
+
+  {
+    // type_traits? decltype??? decltype!
+    static_assert(std::is_same_v<decltype(ekin), Energy>);
+
+    using Accel = decltype(1_m / (1_s * 1_s));
+    Force f = 2_kg * Accel{5};
+  }
+
+  constexpr auto GHz = 1'000'000'000 / 1_s;
+  constexpr auto ns = 1_s / 1'000'000'000;
+
+  const auto cpu_clock = 4 * GHz;
+  auto memory_latency = 70 * ns;
+  std::cout << "clock cycles wasted: " << double(cpu_clock * memory_latency) << '\n';
+
   return 0;
 }
